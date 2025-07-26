b3734447cf9af63ff246b34c1be65fdb49276528
kartik1502 <kartikkulkarni1411@gmail.com>
2025-07-26 17:47:44 +0530

    feat: add service-registry module

---
+++ src/main/java/org/arya/banking/service/registry/AryaBankingServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package org.arya.banking.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AryaBankingServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AryaBankingServiceRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/arya/banking/service/registry/AryaBankingServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arya.banking.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AryaBankingServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
